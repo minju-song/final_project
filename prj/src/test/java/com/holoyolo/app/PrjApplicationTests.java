1e43b56dbd5958e9a08391e7c7925827c5e038c8
Song Min Ju <97097194+minju-song@users.noreply.github.com>
2023-11-20 17:03:57 +0900

    init

--- prj/src/test/java/com/holoyolo/app/PrjApplicationTests.java
+++ prj/src/test/java/com/holoyolo/app/PrjApplicationTests.java
@@ -1,13 +1,34 @@
 package com.holoyolo.app;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PrjApplicationTests {
 
+	@Autowired
+	StringEncryptor jasyptStringEncryptor;
+	
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	void encryptor() {
+		String[] datas = {
+				"oracle.jdbc.OracleDriver"
+				,"jdbc:oracle:thin:@127.0.0.1:1521/xe"
+				,"hr"
+				,"hr"
+		};
+		
+		for(String data : datas) {
+			String encData = jasyptStringEncryptor.encrypt(data);
+			System.out.println(data +" >>> "+encData);
+		}
+	}
+
 
 }
