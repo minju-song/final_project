62f25ffb8264d84ceb9e8f284b603f6ac8540080
CHUNcarry <chuncarry00@gmail.com>
2023-11-21 17:51:48 +0900

    날짜적용

--- prj/src/main/java/com/holoyolo/app/holopayHistory/service/Impl/HoloPayHistoryServiceImpl.java
+++ prj/src/main/java/com/holoyolo/app/holopayHistory/service/Impl/HoloPayHistoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.holoyolo.app.holopayHistory.service.Impl;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
@@ -50,39 +51,47 @@ public class HoloPayHistoryServiceImpl implements HoloPayHistoryService {
 		
 		String IsTuno = String.valueOf(System.currentTimeMillis()/1000+((int) Math.random()*9+1)) ;
 		
-//		LocalTime hour = loca
+		LocalTime now = LocalTime.now();
+		LocalDate date = LocalDate.now();
 		
+		String year = String.valueOf(date.getYear());
+		String mon = String.valueOf(now.getMinute());
+		String day = String.valueOf(now.getSecond());
+		String hour = String.valueOf(now.getHour());
+		String min = String.valueOf(now.getMinute());
+		String sec = String.valueOf(now.getSecond());
 		
 		// Request Header 설정
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
-		headers.set("ApiNm","DrawingTransfer");
-		headers.set("Tsymd","");
-		headers.set("Trtm","DrawingTransfer");
+		
+		JSONObject requestBody = new JSONObject();
+		requestBody.put("key", "value");
+		requestBody.put("ApiNm","DrawingTransfer");
+		headers.set("Tsymd",year+mon+day);
+		headers.set("Trtm",hour+min+sec);
 		headers.set("Iscd",IsTuno);
 		headers.set("FintechApsno","001");
 		headers.set("ApiSvcCd","DrawingTransferA");
 		headers.set("IsTuno","DrawingTransfer");
-		
-//		{
-//			  "Header": {
-//			    "ApiNm": "DrawingTransfer",
-//			    "Tsymd": "오늘날짜를입력하세요",
-//			    "Trtm": "112428",
-//			    "Iscd": "기관코드를입력하세요",
-//			    "FintechApsno": "001",
-//			    "ApiSvcCd": "DrawingTransferA",
-//			    "IsTuno": "0000",
-//			    "AccessToken": "인증키를입력하세요"
-//			  },
-//			  "FinAcno": "핀어카운트",
-//			  "Tram": "거래금액",
-//			  "DractOtlt": "출금계좌인자내용"
-//			}
-		// Request Body 설정
-		JSONObject requestBody = new JSONObject();
-		requestBody.put("key", "value");
 
+//		{	
+//		    "Header":{
+//		        "ApiNm":"DrawingTransfer",
+//		        "Tsymd":"20191129",
+//		        "Trtm":"125237",
+//		        "Iscd":"900001",
+//		        "FintechApsno":"001",
+//		        "ApiSvcCd":"DrawingTransferA",
+//		        "IsTuno":"201911290000000001", 
+//		        "AccessToken": "6500e3a81fe2deafd996ea437b6a4b7cfbd04a3ab7e26480b431fdf3ccf3b39b"
+//		    },
+//		"FinAcno":"00820109000010001413",
+//		"Tram":"1000000",
+//		"DractOtlt":"테스트",
+//		"MractOtlt":"테스트"
+//		}
+//		Response example
 		// Request Entity 생성
 		HttpEntity<String> entity = new HttpEntity<String>(requestBody.toString(), headers);
 	}
