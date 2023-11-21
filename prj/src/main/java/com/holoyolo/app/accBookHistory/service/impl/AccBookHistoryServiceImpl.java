1af9ada0422cebdfb0ee5cf659f1e1c8b9622e62
Song Min Ju <97097194+minju-song@users.noreply.github.com>
2023-11-21 09:57:19 +0900

    init

--- prj/src/main/java/com/holoyolo/app/accBookHistory/service/impl/AccBookHistoryServiceImpl.java
+++ prj/src/main/java/com/holoyolo/app/accBookHistory/service/impl/AccBookHistoryServiceImpl.java
@@ -1,6 +1,6 @@
 package com.holoyolo.app.accBookHistory.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import com.holoyolo.app.accBookHistory.mapper.AccBookHistoryMapper;
@@ -9,6 +9,5 @@ import com.holoyolo.app.accBookHistory.service.AccBookHistoryService;
 @Service
 public class AccBookHistoryServiceImpl implements AccBookHistoryService {
 	
-	@Autowired
-	AccBookHistoryMapper accBookHistoryMapper;
+
 }
--- prj/src/main/java/com/holoyolo/app/review/service/impl/ReviewServiceImpl.java
+++ prj/src/main/java/com/holoyolo/app/review/service/impl/ReviewServiceImpl.java
@@ -4,9 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.holoyolo.app.review.mapper.ReviewMapper;
+import com.holoyolo.app.review.service.ReviewService;
 
 @Service
-public class ReviewServiceImpl {
+public class ReviewServiceImpl implements ReviewService{
 	
 	@Autowired
 	ReviewMapper reviewMapper;
