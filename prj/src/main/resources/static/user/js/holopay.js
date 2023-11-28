d433e74e32622d3ccc10b7573bbdd0dbbb9e1523
CHUNcarry <chuncarry00@gmail.com>
2023-11-29 01:21:11 +0900

    내역조회 페이징 중

--- prj/src/main/resources/static/user/js/holopay.js
+++ prj/src/main/resources/static/user/js/holopay.js
@@ -146,48 +146,64 @@ let search = document.getElementById('searchHpHistory');
 search.addEventListener('change', loadData);
 
 function loadData() {
-  console.log('asda');
-let term = $("#searchHpHistory").val();
-
-console.log(term);
+  let term = $("#searchHpHistory").val();
   $.ajax({
     type: 'POST',
     url: '/loadhistory',
     contentType: 'application/json;charset=UTF-8',
     data: JSON.stringify({ "term": term }),
     success: function (data) {
-      console.log(data)
-      // 성공 시 데이터를 이용해 목록을 동적으로 생성하는 로직 추가
+
       let tbody = $("#hpHistoryTable tbody");
       tbody.empty(); // 기존 데이터를 지우고 새로운 데이터로 갱신
 
       if (data && data.length > 0) {
         // 데이터가 있을 경우 테이블에 행 추가
-        data.forEach(function (item) {
+        data.forEach(function (item, index) {
           let row = $("<tr>");
-          row.append($("<td>").text(item.hpHistoryId));
+          row.append($("<td>").text(index + 1));
+          row.append($("<td>").text(getTransactionType(item.hpType)));
           row.append($("<td>").text(item.price));
           row.append($("<td>").text(item.holopayComment));
-          row.append($("<td>").text(item.hpDate)); // 날짜 형식에 따라 수정
+          row.append($("<td>").text(formatDate(item.hpDate)));
 
           tbody.append(row);
         });
       } else {
         // 데이터가 없을 경우 테이블에 메시지 추가
-        let noDataMessage = $("<tr>").append($("<td colspan='4' class='text-center'>").text("거래내역 없음"));
+        let noDataMessage = $("<tr>").append($("<td colspan='5' class='text-center'>").text("거래내역 없음"));
         tbody.append(noDataMessage);
       }
     },
     error: function (request, status, error) {
       // Handle the error response from the server
-      console.error("code: " + request.status)
-      console.error("message: " + request.responseText)
+      console.error("code: " + request.status);
+      console.error("message: " + request.responseText);
       console.error("error: " + error);
     }
   });
+
 }
 
+function formatDate(dateString) {
+  let date = new Date(dateString);
+  let year = date.getFullYear();
+  let month = (date.getMonth() + 1).toString().padStart(2, '0');
+  let day = date.getDate().toString().padStart(2, '0');
+  return year + '-' + month + '-' + day;
+}
 
+function getTransactionType(type) {
+  switch (type) {
+    case 'HP1':
+      return '충전';
+    case 'HP2':
+      return '인출';
+    // 다른 유형에 대한 처리 추가 가능
+    default:
+      return type;
+  }
+}
 
 
 
