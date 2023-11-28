2050f987fb8b9a4a076f1a5e72b1a288c103f378
전유진(Jeon Yu Jin) <sok030614@gmail.com>
2023-11-28 19:08:54 +0900

    필요없는 내용 삭제

--- prj/src/main/java/com/holoyolo/app/config/SecurityConfig.java
+++ prj/src/main/java/com/holoyolo/app/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.holoyolo.app.auth.PrincipalOauth2UserService;
