bc6ef3d7c3f0592cd06c539e20a9758171fd7a82
전유진(Jeon Yu Jin) <sok030614@gmail.com>
2023-12-04 01:34:08 +0900

    회원탈퇴 수정중(암호화 관련해서 확인)

--- prj/src/main/java/com/holoyolo/app/member/web/MemberController.java
+++ prj/src/main/java/com/holoyolo/app/member/web/MemberController.java
@@ -2,6 +2,7 @@ package com.holoyolo.app.member.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
