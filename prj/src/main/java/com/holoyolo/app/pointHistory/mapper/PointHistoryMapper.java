e44967166797caa30f19c358ab4b993127545b8b
Song Min Ju <97097194+minju-song@users.noreply.github.com>
2023-12-08 17:23:46 +0900

    마이페이지 작업중

--- prj/src/main/java/com/holoyolo/app/pointHistory/mapper/PointHistoryMapper.java
+++ prj/src/main/java/com/holoyolo/app/pointHistory/mapper/PointHistoryMapper.java
@@ -1,8 +1,14 @@
 package com.holoyolo.app.pointHistory.mapper;
 
+import java.util.List;
+
 import com.holoyolo.app.member.service.MemberVO;
+import com.holoyolo.app.pointHistory.service.PointHistoryVO;
 
 public interface PointHistoryMapper {
 	//포인트 조회
 	public int pointBalance(MemberVO vo);
+	
+	//포인트내역조회
+	public List<PointHistoryVO> getPointHistory(MemberVO vo);
 }
--- prj/src/main/java/com/holoyolo/app/pointHistory/web/PointHistoryController.java
+++ prj/src/main/java/com/holoyolo/app/pointHistory/web/PointHistoryController.java
@@ -1,5 +1,7 @@
 package com.holoyolo.app.pointHistory.web;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -18,7 +20,9 @@ public class PointHistoryController {
 	//마이페이지 포인트내역 페이지
 	@GetMapping("/member/myPoint")
 	public String myPointPage(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
+		Map<String, Object> map = pointHistoryService.pageMyPoint(principalDetails.getMemberVO());
 		
+		model.addAttribute("result", map);
 		model.addAttribute("user", principalDetails.getMemberVO());
 		model.addAttribute("menu", "mypage");
 		return "user/mypage/myPoint";
