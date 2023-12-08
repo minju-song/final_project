7ccea3fbccb34af188390e71edc64ee71652af6b
Song Min Ju <97097194+minju-song@users.noreply.github.com>
2023-12-08 18:31:07 +0900

    .

--- prj/src/main/java/com/holoyolo/app/clubSuccessHistory/web/ClubSuccessHistoryController.java
+++ prj/src/main/java/com/holoyolo/app/clubSuccessHistory/web/ClubSuccessHistoryController.java
@@ -1,8 +1,17 @@
 package com.holoyolo.app.clubSuccessHistory.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.holoyolo.app.auth.PrincipalDetails;
+import com.holoyolo.app.club.service.ClubService;
+import com.holoyolo.app.club.service.ClubVO;
 import com.holoyolo.app.clubSuccessHistory.service.ClubSuccessHistoryService;
 
 @Controller
@@ -10,4 +19,22 @@ public class ClubSuccessHistoryController {
 	
 	@Autowired
 	ClubSuccessHistoryService clubSuccessHistoryService;
+	
+	@Autowired
+	ClubService clubService;
+	
+	@GetMapping("/member/club/clubHistory")
+	public String clubHistoryPage(@AuthenticationPrincipal PrincipalDetails principalDetails,Model model,ClubVO vo) {
+		Map<String, Object> map = new HashMap<>();
+		System.out.println(vo);
+		
+		vo = clubService.getClub(vo.getClubId());
+		
+		map.put("club", vo);
+		
+		model.addAttribute("result", map);
+		model.addAttribute("menu", "club");
+		model.addAttribute("subMenu", "history");
+		return "user/club/clubHistory";
+	}
 }
